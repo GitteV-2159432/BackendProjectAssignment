8d0afe747487309c0ed2d3ac646ae6eea7775d02
fde8413 <fde8413@students.fhv.at>
2025-04-28 19:24:38 +0300

    bookmark server & bookmark routes

---
+++ server/services/bookmark-service.js
@@ -0,0 +1,79 @@
+import createGenericService from './components/generic-service.js'
+import userService from './user-service.js'
+import HttpError from '../utils/httpError.js'
+
+const createBookmarkService = (Model) => {
+  const modelName = Model.modelName || 'Document'
+  const modelNamePl = `${modelName}s`
+  const modelService = createGenericService(Model)
+
+  const getModelBookmarkIds = async (modelId, userId) => {
+    const document = await modelService.getById(modelId)
+
+    if (!document.isPublic) {
+      throw new HttpError(
+        400,
+        `Only public ${modelNamePl.toLowerCase()} can be bookmarked.`
+      )
+    }
+
+    return (await userService.getById(userId)).bookmarks[
+      modelNamePl.toLowerCase()
+    ]
+  }
+
+  const setModifiedModelBookmarkIds = async (
+    modifiedModelBookmarkIds,
+    userId
+  ) => {
+    await userService.update(userId, {
+      bookmarks: {
+        [modelNamePl.toLowerCase()]: modifiedModelBookmarkIds,
+      },
+    })
+  }
+
+  return {
+    async setBookmark(modelId, userId) {
+      const modelBookmarkIds = await getModelBookmarkIds(modelId, userId)
+      const modifiedModelBookmarkIds = [...modelBookmarkIds, modelId]
+
+      await setModifiedModelBookmarkIds(modifiedModelBookmarkIds, userId)
+
+      return modelService.getAll(
+        {
+          isPublic: true,
+          _id: {
+            $nin: (await userService.getById(userId)).bookmarks[
+              modelNamePl.toLowerCase()
+            ],
+          },
+        },
+        { name: 1 }
+      )
+    },
+
+    async removeBookmark(modelId, userId) {
+      const modelBookmarkIds = await getModelBookmarkIds(modelId, userId)
+      const modifiedModelBookmarkIds = modelBookmarkIds.filter(
+        (id) => !id.equals(modelId)
+      )
+
+      await setModifiedModelBookmarkIds(modifiedModelBookmarkIds, userId)
+
+      return modelService.getAll(
+        {
+          isPublic: true,
+          _id: {
+            $in: (await userService.getById(userId)).bookmarks[
+              modelNamePl.toLowerCase()
+            ],
+          },
+        },
+        { name: 1 }
+      )
+    },
+  }
+}
+
+export default createBookmarkService
