55f3a5e24aa95453aa832b2a1fbe4231e89f1527
Der eloquente Elias <eso7445@students.fhv.at>
2025-04-29 22:53:57 +0300

    feat(formatAndCleanup) - formats files - removes unneccesary template stuff

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,13 @@
-import Navbar from "../components/NavBar";
-import { useEffect, useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import Navbar from '../components/NavBar'
+import { useEffect, useState } from 'react'
+import { useAuth } from '../context/AuthContext'
 
 const Dashboard = () => {
- 
-
   return (
     <div>
       <h1>Dashboard</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,42 +1,50 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
-import Navbar from "../components/NavBar";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+import Navbar from '../components/NavBar'
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { login } = useAuth();
-  const navigate = useNavigate();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { login } = useAuth()
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const res = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-    });
-      
+    const res = await fetch('http://localhost:5000/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    })
 
-    const data = await res.json();
+    const data = await res.json()
     if (res.ok) {
-      login(data.token);
-      navigate("/dashboard");
+      login(data.token)
+      navigate('/dashboard')
     } else {
-      alert(data.message);
+      alert(data.message)
     }
-  };
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <Navbar></Navbar>
       <h2>Login</h2>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-      <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        placeholder="Password"
+      />
       <button type="submit">Login</button>
     </form>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
