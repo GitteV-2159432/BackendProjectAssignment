f47250f33189fce6e6df5295806d6ec0889ec384
Gitte <gitte.vandevenne@student.uhasselt.be>
2025-04-30 14:27:00 +0300

    fix navbar style

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/navbar/NavBar.jsx";
 
 const Layout = () => {
     return (
-        <div className = "App">
+        <div style={{ display: 'flex', minHeight: '100vh' }}>
             <Navbar />
             <Outlet />
         </div>  
