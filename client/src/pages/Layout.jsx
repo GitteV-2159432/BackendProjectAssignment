268a9c7b2e3baecb09fc5995abdf10513797287e
Gitte <gitte.vandevenne@student.uhasselt.be>
2025-04-30 14:24:21 +0300

    fix nav routes

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar/NavBar.jsx";
+
+const Layout = () => {
+    return (
+        <div className = "App">
+            <Navbar />
+            <Outlet />
+        </div>  
+    );
+};
+
+export default Layout;
