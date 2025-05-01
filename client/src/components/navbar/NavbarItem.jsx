6420efdff0e40f57f5ab087c7e995381da011b79
Der eloquente Elias <eso7445@students.fhv.at>
2025-05-01 21:59:14 +0300

    feat(fixes) - fixes errors thrown by eslint - changes for to htmlFor in .jsx files

--- client/src/components/navbar/NavbarItem.jsx
+++ client/src/components/navbar/NavbarItem.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 
-const NavbarItem = ({ title, icon: Icon, to }) => {
+const NavbarItem = ({ title, icon, to }) => {
   return (
     <NavLink to={to} className="block">
       {({ isActive }) => (
@@ -9,7 +9,7 @@ const NavbarItem = ({ title, icon: Icon, to }) => {
             className={`px-6 py-2 rounded-full 
               ${isActive ? 'bg-[#C297B8]/50' : ''}`}
           >
-            <Icon isActive={isActive} />
+            {icon({ isActive })}
           </div>
           <span className={'text-sm font-semibold'}>{title}</span>
         </div>
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from 'react'
+import { createContext, useState } from 'react'
 
 const AuthContext = createContext()
 
@@ -23,4 +23,4 @@ export const AuthProvider = ({ children }) => {
   )
 }
 
-export const useAuth = () => useContext(AuthContext)
+export default AuthContext
