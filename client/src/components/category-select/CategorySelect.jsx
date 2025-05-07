920fa974788e426c58c0b97604f38e237dd5ba50
Der eloquente Elias <eso7445@students.fhv.at>
2025-05-07 12:58:16 +0300

    feat(addLabelsAndAttributes) - adds labels for input fields - adds attributes to input fields - adds label for exercise category select

--- client/src/components/category-select/CategorySelect.jsx
+++ client/src/components/category-select/CategorySelect.jsx
@@ -6,15 +6,19 @@ const CategorySelect = ({ categories, setCurrentCategory }) => {
   }
 
   return (
-    <select
-      className="bg-[#070707] border-2 border-[#FAF9F666] rounded-full px-4 py-2 text-[#FAF9F6] text-sm"
-      onChange={handleChange}
-    >
-      {categories &&
-        categories.map((cat) => {
-          return <SelectOption key={cat.id} id={cat.id} name={cat.name} />
-        })}
-    </select>
+    <>
+      <label htmlFor="exerciseCategorySelect">Exercise category</label>
+      <select
+        id="exerciseCategorySelect"
+        className="bg-[#070707] border-2 border-[#FAF9F666] rounded-full px-4 py-2 text-[#FAF9F6] text-sm"
+        onChange={handleChange}
+      >
+        {categories &&
+          categories.map((cat) => {
+            return <SelectOption key={cat.id} id={cat.id} name={cat.name} />
+          })}
+      </select>
+    </>
   )
 }
 
